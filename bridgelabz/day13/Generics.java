902411ae8eb7e4ae75033f9ab31cbc83d2fb62c7
Bharti <bharatifule10gmail.com>
2023-01-23 00:25:53 +0530

    Added:Generics Problems

--- bridgelabz/day13/Generics.java
+++ bridgelabz/day13/Generics.java
@@ -1,34 +1,27 @@
 package com.bridgelabz.day13;
 
-public class Generics <T extends Comparable> {
-    T[] array;
+public class Generics  {
+	static <P extends Comparable> void max_variables( P str1, P str2, P str3){
 
-    Generics(T[] arr1) {
-        this.array = arr1;
-    }
-
-    void max_variables(){
-        T largest = this.array[0];
-        for (int i=0; i<this.array.length; i++){
-            if(this.array[i].compareTo(largest) > 0){
-                largest = this.array[i];
-                this.array[i] = largest;
+        if(str1.compareTo(str2) > 0){
+            if(str1.compareTo(str3) > 0){
+                System.out.println( str1 + " is Maximum of three given inputs ");
+            }else {
+                System.out.println( str3 + " is Maximum of three given inputs  ");
+            }
+        }else{
+            if(str2.compareTo(str3) > 0){
+                System.out.println( str2 + " is Maximum of three given inputs  ");
+            }else{
+                System.out.println( str3 + " is Maximum of three given inputs  ");
             }
         }
-        System.out.println(" Largest element in the array is " + largest);
     }
 
+
     public static void main(String[] args) {
-        Integer[] int_arr = {14,77,11,1,56};
-        Generics <Integer> obj1 = new Generics(int_arr);
-        obj1.max_variables();
-
-        String[] st_arr = {"Banana", "Apple", "Orange","Pineapple","Greps"};
-        Generics <String> obj2 = new Generics(st_arr);
-        obj2.max_variables();
-
-        Float[] float_arr = {25.1f, 4.3f, 7.1f};
-        Generics <Float> obj3 = new Generics(float_arr);
-        obj3.max_variables();
+        max_variables(10,5000,99);
+        max_variables(7.1f,119.8f,3.0f);
+        max_variables("Orange", "WaterMelon", "Banana");
     }
 }
